5fa547d8faab537b2ad167bddc4160a8a8b671f7
Federico Pizarro <127800142+fedepds@users.noreply.github.com>
2024-09-18 21:00:20 -0300

    TAtermindo

--- src/App.js
+++ src/App.js
@@ -1,13 +1,28 @@
-import logo from './logo.svg';
 import './App.css';
+import { useState, useEffect } from 'react';
+import logo from './logo.svg'; // Asegúrate de importar tu logo
 
 function App() {
+  const [count, setCount] = useState(0);
+
+  useEffect(() => {
+    const timer = setInterval(() => {
+      setCount((prevCount) => prevCount + 1);
+    }, 1000);
+
+ 
+    return () => clearInterval(timer);
+  }, []);
+
   return (
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Edit <code>src/App.js</code> and save to reload.
+          Temporizador: {count} segundos
+        </p>
+        <p>
+          Edita <code>src/App.js</code> y guarda para recargar.
         </p>
         <a
           className="App-link"
@@ -15,7 +30,7 @@ function App() {
           target="_blank"
           rel="noopener noreferrer"
         >
-          Learn React
+          Aprende React
         </a>
       </header>
     </div>
